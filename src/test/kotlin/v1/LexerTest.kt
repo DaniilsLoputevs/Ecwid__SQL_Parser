305422661b4df87a90f8f50998cce3ac3f3c54a1
DaniilsLoputevs <laiwiense@gmail.com>
2023-09-24 20:14:46 +0300

    init commit + move to this repo + add some tests

---
+++ src/test/kotlin/v1/LexerTest.kt
@@ -0,0 +1,56 @@
+package v1
+
+import io.kotest.core.spec.style.FunSpec
+import v1.TokenType.*
+
+/**
+ * testcases:
+ * + "SELECT * from users;" // canonical
+ * - "SELECT 'text_text_value'" // text value
+ * - "SELECT now()" // func value
+ * - "SELECT 10" // number value
+ * + "SELECT * from users" // no semicolon
+ * + "SELECT id from users" // no semicolon + one field
+ * - "SELECT id, name from users" // no semicolon + 2 fields field
+ * - "SELECT users.id, users.name from users" // no semicolon + 2 fields field + table.field
+ * - "SELECT u.id, u.name from users u" // no semicolon + 2 fields field + alias.field
+ * - "SELECT COUNT(*) users;" // func + arg: star
+ * - "SELECT COUNT(id) users;" // func + arg: column
+ * - "SELECT CONCAT('Hello, ', name) users;" // func + arg: string value + column
+ *
+ *
+ * - "SELECT    id,\r  name  \r\n  from \n users" // no semicolon + 2 fields field + space & tab chars
+ */
+class LexerTest : FunSpec({
+    val test = Lexer()
+    val debug = LexerDebug()
+
+    test("canonical") {
+        testByAllContainsOrdered(
+            test, "SELECT * from users;",
+            tokenStub(type = SELECT), tokenStub(type = SPACE),
+            tokenStub(type = STAR), tokenStub(type = SPACE),
+            tokenStub(type = FROM), tokenStub(type = SPACE),
+            tokenStub(type = EXPRESSION),
+            tokenStub(type = SEMICOLON),
+        )
+    }
+    test("no semicolon") {
+        testByAllContainsOrdered(
+            test, "SELECT * from users",
+            tokenStub(type = SELECT), tokenStub(type = SPACE),
+            tokenStub(type = STAR), tokenStub(type = SPACE),
+            tokenStub(type = FROM), tokenStub(type = SPACE),
+            tokenStub(type = EXPRESSION),
+        )
+    }
+    test("no semicolon + one field") {
+        testByAllContainsOrdered(
+            test, "SELECT id from users",
+            tokenStub(type = SELECT), tokenStub(type = SPACE),
+            tokenStub(type = EXPRESSION), tokenStub(type = SPACE),
+            tokenStub(type = FROM), tokenStub(type = SPACE),
+            tokenStub(type = EXPRESSION),
+        )
+    }
+})
---
+++ src/test/kotlin/v1/LexerTestSupport.kt
@@ -0,0 +1,91 @@
+package v1
+
+import debug.PrintTable
+import io.kotest.core.test.TestScope
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.should
+import java.util.*
+
+class LexerDebug : Lexer() {
+    override fun analyse(sql: String): Queue<Token> {
+        return super.analyse(sql).also { it.printLexAnalyse(sql) }
+    }
+}
+fun tokenStub(
+    startIndex: Int = -1,
+    endIndex: Int = -1,
+    text: String = "",
+    type: TokenType
+) = Token(startIndex, endIndex, text, type)
+
+fun TestScope.testByAllContainsOrdered(
+    lexer : Lexer,
+    sql : String,
+    vararg expected: Token,
+) {
+    val actual = lexer.analyse(sql)
+
+    val actualIter = actual.iterator()
+    val expectedIter = expected.iterator()
+
+    actual shouldHaveSize (expected.size)
+
+    while (actualIter.hasNext()) {
+        val token = actualIter.next()
+        val expect = expectedIter.next()
+        token should (TypeMatcher(expect))
+        if (expect.text.isNotEmpty()) token should (TextMatcher(expect))
+        if (expect.startIndex != -1) token should (StartIndexMatcher(expect))
+        if (expect.endIndex != -1) token should (EndIndexMatcher(expect))
+    }
+}
+
+class TypeMatcher(private val token: Token) : Matcher<Token> {
+    override fun test(value: Token) = MatcherResult.Companion.invoke(
+        value.type == token.type,
+        { "mismatch Token.type" },
+        { "mismatch Token.type" }
+    )
+}
+
+class TextMatcher(private val token: Token) : Matcher<Token> {
+    override fun test(value: Token): MatcherResult {
+        return MatcherResult.Companion.invoke(
+            value.text == token.text,
+            { "mismatch Token.text" },
+            { "mismatch Token.text" })
+    }
+}
+
+class StartIndexMatcher(private val token: Token) : Matcher<Token> {
+    override fun test(value: Token): MatcherResult {
+        return MatcherResult.Companion.invoke(
+            value.startIndex == token.startIndex,
+            { "mismatch Token.startIndex" },
+            { "mismatch Token.startIndex" })
+    }
+}
+
+class EndIndexMatcher(private val token: Token) : Matcher<Token> {
+    override fun test(value: Token): MatcherResult {
+        return MatcherResult.Companion.invoke(
+            value.endIndex == token.endIndex,
+            { "mismatch Token.endIndex" },
+            { "mismatch Token.endIndex" })
+    }
+}
+
+
+fun Collection<Token>.printLexAnalyse(originalSql : String) : Collection<Token> = this.also {
+    PrintTable.of(this)
+        .columnElemIndex()
+        .column("TYPE", Token::type)
+        .column("START", Token::startIndex)
+        .column("END", Token::endIndex)
+        .column("S-CHAR") { originalSql[it.startIndex] }
+        .column("E-CHAR") { originalSql[it.endIndex] }
+        .column("TEXT", Token::text)
+        .print()
+}
